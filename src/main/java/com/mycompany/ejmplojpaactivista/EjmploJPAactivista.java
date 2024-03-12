317d16d9d44c72c87f0fb52c05c61087538251b4
pmoyac <48139060+pmoyac@users.noreply.github.com>
2024-03-12 11:37:31 -0700

    initial commit

---
+++ src/main/java/com/mycompany/ejmplojpaactivista/EjmploJPAactivista.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejmplojpaactivista;
+
+import java.util.ArrayList;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.GregorianCalendar;
+import java.util.List;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
+import objetos.Activista;
+import objetos.Cliente;
+import objetos.Direccion;
+import objetos.Problema;
+
+/**
+ *
+ * @author Laboratorios
+ */
+public class EjmploJPAactivista {
+
+    public static void main(String[] args) {
+        
+        EntityManagerFactory mf = Persistence.createEntityManagerFactory("unidadPersistencia");
+        EntityManager em = mf.createEntityManager();
+        em.getTransaction().begin();
+                
+        //Crear activista
+//        Activista activista = new Activista("Pedro", "Moya", "Castro", "64412456", new GregorianCalendar(2020, 1, 15));
+//        
+//        em.persist(activista);
+//        em.getTransaction().commit();
+        
+    //Buscar y Actualizar activista
+//        Activista a = em.find(Activista.class, 2L);
+//        
+//        if(a!=null){
+//            a.setAp_materno("Halpert");
+//            a.setTelefono("64448452");
+//            em.persist(a);
+////            System.out.println(a.toString());
+//        }else{
+//            System.out.println("activista no encontrado");
+//        }
+//        em.getTransaction().commit();
+
+// Eliminar activista
+//        Activista a = em.find(Activista.class, 3L);
+//        
+//        if (a!=null) {
+//            em.remove(a);
+//            System.out.println("activista eliminado");
+//        }else{
+//            System.out.println("activista no encontrado");
+//        }
+//        
+//        em.getTransaction().commit();
+//  
+
+//Consiiltar tabla
+//        CriteriaQuery criteria = em.getCriteriaBuilder().createQuery();
+//        
+//        criteria.select(criteria.from(Activista.class));
+//        Query query = em.createQuery(criteria);
+//        List<Activista> activistas = query.getResultList();
+//        
+//        for (Activista activista: activistas) {
+//            System.out.println(activista.toString());
+//        }
+//        em.getTransaction().commit();
+
+//        Cliente c = new Cliente();
+//        c.setNombres("juan");
+//        c.setAp_materno("lopez");
+//        c.setAp_paterno("rsamirezx");
+//        Direccion d = new Direccion("wallby", "villa", 85000L, 548L, c);
+//                
+//        List<Direccion> ds = new ArrayList();
+//
+//        ds.add(d);
+//        c.setDirecciones(ds);
+//        em.persist(c);
+//        em.getTransaction().commit();
+
+        Cliente c = new Cliente();
+        c.setNombres("jim");
+        c.setAp_materno("halpert");
+        c.setAp_paterno("rsamirezx");
+        Activista a = new Activista("pedro","moya","asdg","65465465",new GregorianCalendar(2020, 1, 15));
+        Problema p = new Problema(new GregorianCalendar(2020, 1, 15),new GregorianCalendar(2020, 1, 15),"pendiente");
+        
+        
+        
+    }
+
+
+}
---
+++ src/main/java/objetos/Problema.java
@@ -0,0 +1,156 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package objetos;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.*;
+
+/**
+ *
+ * @author Laboratorios
+ */
+@Entity
+@Table(name="problema")
+public class Problema implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Calendar fechaInicio;
+    private Calendar fechaFinal;
+    private String estado;
+    
+    @ManyToOne
+    @JoinColumn(name = "idCliente")
+    private Cliente cliente;
+    
+    @ManyToMany(mappedBy = "problemas")
+    List<Activista> activistas;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Calendar getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Calendar fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Calendar getFechaFinal() {
+        return fechaFinal;
+    }
+
+    public void setFechaFinal(Calendar fechaFinal) {
+        this.fechaFinal = fechaFinal;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public List<Activista> getActivistas() {
+        return activistas;
+    }
+
+    public void setActivistas(List<Activista> activistas) {
+        this.activistas = activistas;
+    }
+
+    
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Problema(Calendar fechaInicio, Calendar fechaFinal, String estado, Cliente cliente, List<Activista> activistas) {
+        this.fechaInicio = fechaInicio;
+        this.fechaFinal = fechaFinal;
+        this.estado = estado;
+        this.cliente = cliente;
+        this.activistas = activistas;
+    }
+    
+    
+
+    public Problema() {
+    }
+
+    public Problema(Calendar fechaInicio, Calendar fechaFinal, String estado) {
+        this.fechaInicio = fechaInicio;
+        this.fechaFinal = fechaFinal;
+        this.estado = estado;
+    }
+
+    public Problema(Long id, Calendar fechaInicio, Calendar fechaFinal, String estado) {
+        this.id = id;
+        this.fechaInicio = fechaInicio;
+        this.fechaFinal = fechaFinal;
+        this.estado = estado;
+    }
+
+    public Problema(Long id, Calendar fechaInicio, Calendar fechaFinal, String estado, List<Activista> activistas) {
+        this.id = id;
+        this.fechaInicio = fechaInicio;
+        this.fechaFinal = fechaFinal;
+        this.estado = estado;
+        this.activistas = activistas;
+    }
+    
+    
+    
+    
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Problema)) {
+            return false;
+        }
+        Problema other = (Problema) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Problema{" + "id=" + id + ", fechaInicio=" + fechaInicio.getTime() + ", fechaFinal=" + fechaFinal.getTime() + ", estado=" + estado + '}';
+    }
+
+    
+    
+    
+}
